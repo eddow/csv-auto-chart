75b37d9b65a76e5fb24b9895ec8f8a5fcc3f4d41
fmdm <eeddow@gmail.com>
2018-04-13 22:34:18 +0300

    css first-launch 'default' folder appearing bug

--- fuse.js
+++ fuse.js
@@ -3,7 +3,7 @@ const {FuseBox, WebIndexPlugin, VueComponentPlugin, JSONPlugin, CSSPlugin} = req
 const fuse = FuseBox.init({
     homeDir : "src",
     target : 'browser@es6',
-    output : "dist/$name.js",
+		output : "dist/$name.js",
     plugins : [
         WebIndexPlugin({
 				title: 'csv-auto-chart',
@@ -11,14 +11,15 @@ const fuse = FuseBox.init({
 			}),
 			VueComponentPlugin(),
 			JSONPlugin(),
-			CSSPlugin({
+			CSSPlugin(/*{
 				group: 'index.css',
 				outFile: 'dist/index.css'
-			})
+			}*/)
     ],
     alias: {
 			"vue": "vue/dist/vue.esm.js",
-    },
+			"vue-router": "vue-router/dist/vue-router.esm.js"
+		},
 		shim: {
 				 jquery: {
 						 source: "node_modules/jquery/dist/jquery.js",
@@ -32,5 +33,5 @@ fuse.dev({
 	port: 8080
 });
 fuse.bundle("vendor").instructions(" ~ index.ts");
-fuse.bundle("index").instructions(" !> [index.ts]").hmr().watch().sourceMaps(true);
+fuse.bundle("index").instructions(" !> [index.ts]").hmr().watch();//.sourceMaps(true);
 fuse.run();
