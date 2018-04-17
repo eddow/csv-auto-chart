e138e371ccde03440295f40947bf68ccd7d870de
fmdm <eeddow@gmail.com>
2018-04-18 02:59:00 +0300

    config file

--- README.md
+++ README.md
@@ -14,3 +14,5 @@ npm install
 # serve with hot reload at localhost:8080
 npm run dev
 ```
+### Fuse config
+`fuse.json` is untracked by git. If it really needs to be changed, [here is the how-to](https://stackoverflow.com/questions/9794931/keep-file-in-a-git-repo-but-dont-track-changes).
--- fuse.js
+++ fuse.js
@@ -1,37 +1,36 @@
 const {FuseBox, WebIndexPlugin, VueComponentPlugin, JSONPlugin, CSSPlugin} = require("fuse-box");
+var config = require('./fuse.json');
 
 const fuse = FuseBox.init({
-    homeDir : "src",
-    target : 'browser@es6',
-		output : "dist/$name.js",
-    plugins : [
-        WebIndexPlugin({
-				title: 'csv-auto-chart',
-				template: 'index.html'
-			}),
-			VueComponentPlugin(),
-			JSONPlugin(),
-			CSSPlugin(/*{
-				group: 'index.css',
-				outFile: 'dist/index.css'
-			}*/)
-    ],
-    alias: {
-			"vue": "vue/dist/vue.esm.js",
-			"vue-router": "vue-router/dist/vue-router.esm.js"
+	homeDir : "src",
+	target : 'browser@es6',
+	output : "dist/$name.js",
+	plugins : [
+		WebIndexPlugin({
+			title: 'csv-auto-chart',
+			template: 'index.html'
+		}),
+		VueComponentPlugin(),
+		JSONPlugin(),
+		CSSPlugin(/*{
+			group: 'index.css',
+			outFile: 'dist/index.css'
+		}*/)
+	],
+	alias: {
+		"vue": "vue/dist/vue.esm.js",
+		"vue-router": "vue-router/dist/vue-router.esm.js"
+	},
+	shim: {
+		jquery: {
+			source: "node_modules/jquery/dist/jquery.js",
+			exports: "$",
 		},
-		shim: {
-				 jquery: {
-						 source: "node_modules/jquery/dist/jquery.js",
-						 exports: "$",
-				 },
-		}
+	},
+	sourceMaps: config.sourceMaps
 })
 
-fuse.dev({
-	open : true,
-	port: 8080
-});
+fuse.dev(config.dev);
 fuse.bundle("vendor").instructions(" ~ index.ts");
-fuse.bundle("index").instructions(" !> [index.ts]").hmr().watch();//.sourceMaps(true);
+fuse.bundle("index").instructions(" !> [index.ts]").hmr().watch();
 fuse.run();
