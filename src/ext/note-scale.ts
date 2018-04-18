3791cfd07c80f16a1b1d90e940ee4d7d89cb2407
fmdm <eeddow@gmail.com>
2018-04-18 20:41:53 +0300

    scale adv

--- src/ext/note-scale.ts
+++ src/ext/note-scale.ts
@@ -1,16 +1,40 @@
 import {Scale} from 'plottable'
 
-export default class noteScale extends Scale<any, number> {
+export default class noteAxis extends Scale<any, number> {
 	//Ugly but gridlines was not written to be extended
   private _mirrorScale: Scale<any, any>;
-  private _extract: (d: any)=> any;
-	
-  constructor(mirrorScale: Scale<any, any>, extract: (d: any)=> any) {
+  private _position: (d: any)=> any;
+  private _info: (d: any)=> any;
+	private _data: any[];
+
+  constructor(mirrorScale: Scale<any, any>, position: (d: any)=> any, info: (d: any)=> any) {
 		super();
     this._mirrorScale = mirrorScale;
-    this._extract = extract;
+    this._position = position;
+    this._info = info;
 	}
+	
+  public domain(): any[];
+  public domain(values: any[]): this;
+  public domain(values?: any[]): any {
+    return super.domain(values);
+	}
+	
+  protected _backingScaleDomain(values?: any[]): any {
+    if (values == null) {
+      return this._data;
+    } else {
+      this._data = values;
+      return this;
+    }
+  }
+  public invertRange(range/*: [number, number] = this.range()*/): any[] {
+		//TODO: Consider range?
+		debugger;
+		return this._data.filter(d=> !!this._info(d));
+  }
+	
 	public scale(value: any): number {
-		return this._mirrorScale.scale(this._extract(value));
+		return this._mirrorScale.scale(this._position(value));
   }
 }
